44f807638a336da731d2268b0e1e4f6c4da44202
jensyfranco <jenmilafran@gmail.com>
2023-05-10 17:06:46 +0200

    start project

---
+++ src/main/java/com/example/mmanalog/MmAnalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.mmanalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmAnalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmAnalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mmanalog/MmAnalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmanalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmAnalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
