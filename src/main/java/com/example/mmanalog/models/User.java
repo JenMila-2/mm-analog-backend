cab784d5ee16a58af002fcf782db7e6c1d847fe7
jensyfranco <jenmilafran@gmail.com>
2023-05-11 12:20:10 +0200

    feat: work on the user model

--- src/main/java/com/example/mmanalog/models/User.java
+++ src/main/java/com/example/mmanalog/models/User.java
@@ -6,6 +6,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Email
+    @Column(nullable = false, unique = true)
+
+    private String name;
+    private String email;
+    private String password;
+    private boolean enabled;
+
 }
