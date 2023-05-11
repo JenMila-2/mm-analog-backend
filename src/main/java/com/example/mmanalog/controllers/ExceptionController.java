558486ca310e859e91c980e18025d5c6fa64b265
jensyfranco <jenmilafran@gmail.com>
2023-05-11 11:12:07 +0200

    feat: work on the exception controller

---
+++ src/main/java/com/example/mmanalog/controllers/ExceptionController.java
@@ -0,0 +1,46 @@
+package com.example.mmanalog.controllers;
+
+import com.example.mmanalog.exceptions.BadRequestException;
+import com.example.mmanalog.exceptions.RecordNotFoundException;
+import com.example.mmanalog.exceptions.IndexOutOfBoundsException;
+import com.example.mmanalog.exceptions.InvalidEmailException;
+import com.example.mmanalog.exceptions.InvalidPasswordException;
+import com.example.mmanalog.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = BadRequestException.class)
+    public ResponseEntity<Object> exception(BadRequestException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<Object> exception(RecordNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = IndexOutOfBoundsException.class)
+    public ResponseEntity<Object> exception(IndexOutOfBoundsException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = InvalidEmailException.class)
+    public ResponseEntity<Object> exception(InvalidEmailException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = InvalidPasswordException.class)
+    public ResponseEntity<Object> exception(InvalidPasswordException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<Object> exception(UserNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
