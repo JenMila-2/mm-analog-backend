c1be2f25967c195d989bbe55a047f74f9e493412
jensyfranco <jenmilafran@gmail.com>
2023-05-11 14:40:48 +0200

    feat: start work on user controller

---
+++ src/main/java/com/example/mmanalog/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.example.mmanalog.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import com.example.mmanalog.exceptions.UserNotFoundException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+
+}
