17f4835ce6f4705169b8148f4d3e226d1c859d2c
jensyfranco <jenmilafran@gmail.com>
2023-05-11 11:44:43 +0200

    chore: update dependencies in pom file

---
+++ src/main/java/com/example/mmanalog/models/User.java
@@ -0,0 +1,11 @@
+package com.example.mmanalog.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+public class User {
+}
