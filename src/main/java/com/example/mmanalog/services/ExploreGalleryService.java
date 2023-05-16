1cceffbe04157ce5fbb5c4020aa447210e10e7ba
jensyfranco <jenmilafran@gmail.com>
2023-05-16 21:17:02 +0200

    feat: add get all photos method

--- src/main/java/com/example/mmanalog/services/ExploreGalleryService.java
+++ src/main/java/com/example/mmanalog/services/ExploreGalleryService.java
@@ -20,4 +20,47 @@ public ExploreGalleryService(ExploreGalleryRepository exploreGalleryRepository) 
     this.exploreGalleryRepository = exploreGalleryRepository;
 }
 
+public List<ExploreGalleryDto> getAllPhotographerGalleries() {
+    Iterable<ExploreGallery> galleries = exploreGalleryRepository.findAll();
+    List<ExploreGalleryDto> galleryDtos = new ArrayList<>();
+
+    for (ExploreGallery gallery : galleries) {
+        galleryDtos.add(transferExploreGalleryToDto(gallery));
+    }
+    return galleryDtos;
+}
+
+public List<ExploreGalleryDto> getAllPhotos() {
+    Iterable<ExploreGallery> explorePhotos = exploreGalleryRepository.findAll();
+    List<ExploreGalleryDto> explorePhotoDtos = new ArrayList<>();
+
+    for (ExploreGallery photo : explorePhotos) {
+        explorePhotoDtos.add(transferExploreGalleryToDto(photo));
+    }
+    return explorePhotoDtos;
+}
+
+public ExploreGallery transferToExploreGallery(ExploreGalleryDto exploreGalleryDto) {
+
+    var exploreGallery = new ExploreGallery();
+
+    exploreGallery.setPhotoId(exploreGalleryDto.getPhotoId());
+    exploreGallery.setPhotographerName(exploreGalleryDto.getPhotographerName());
+    exploreGallery.setPhotoTitle(exploreGalleryDto.getPhotoTitle());
+    exploreGallery.setImageURL(exploreGalleryDto.getImageURL());
+
+    return exploreGallery;
+}
+
+public ExploreGalleryDto transferExploreGalleryToDto(ExploreGallery exploreGallery) {
+    ExploreGalleryDto exploreGalleryDto = new ExploreGalleryDto();
+
+    exploreGalleryDto.photoId = exploreGallery.getPhotoId();
+    exploreGalleryDto.photographerName = exploreGallery.getPhotographerName();
+    exploreGalleryDto.photoTitle = exploreGallery.getPhotoTitle();
+    exploreGalleryDto.imageURL = exploreGallery.getImageURL();
+
+    return exploreGalleryDto;
+}
+
 }
