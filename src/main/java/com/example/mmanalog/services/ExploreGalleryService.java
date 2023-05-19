2d89fa7365362185ece0d17a035cd81e9e9a05b0
jensyfranco <jenmilafran@gmail.com>
2023-05-19 15:19:58 +0200

    feat: work on explore gallery service and controller class

--- src/main/java/com/example/mmanalog/services/ExploreGalleryService.java
+++ src/main/java/com/example/mmanalog/services/ExploreGalleryService.java
@@ -1,6 +1,7 @@
 package com.example.mmanalog.services;
 
 import com.example.mmanalog.dtos.ExploreGalleryDto;
+import com.example.mmanalog.exceptions.RecordNotFoundException;
 import com.example.mmanalog.models.ExploreGallery;
 import com.example.mmanalog.repositories.ExploreGalleryRepository;
 import com.example.mmanalog.exceptions.BadRequestException;
