46c16696487cba97d6168e904b93c9ded4d66da7
jensyfranco <jenmilafran@gmail.com>
2023-05-27 16:34:18 +0200

    feat: add custom query in photo gallery repository to find a photo gallery by the user

--- src/main/java/com/example/mmanalog/repositories/PhotoGalleryRepository.java
+++ src/main/java/com/example/mmanalog/repositories/PhotoGalleryRepository.java
@@ -1,8 +1,12 @@
 package com.example.mmanalog.repositories;
 
 import com.example.mmanalog.models.PhotoGallery;
+import com.example.mmanalog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface PhotoGalleryRepository extends JpaRepository<PhotoGallery, Long> {
 
+    List<PhotoGallery> findPhotoGalleryByUser(User user);
 }
