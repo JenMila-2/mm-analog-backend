cf5866c2d7951b86ae20334cd44b629ba6d5c782
jensyfranco <jenmilafran@gmail.com>
2023-06-13 11:47:45 +0200

    feat: add image upload response and image controller classes for image upload function logic

---
+++ src/main/java/com/example/mmanalog/controllers/ImageController.java
@@ -0,0 +1,57 @@
+package com.example.mmanalog.controllers;
+
+import com.example.mmanalog.models.Image;
+import com.example.mmanalog.repositories.ImageRepository;
+import com.example.mmanalog.utilities.ImageUtility;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin()
+public class ImageController {
+    @Autowired
+    ImageRepository imageRepository;
+
+    @PostMapping("/upload/image")
+    public ResponseEntity<ImageUploadResponse> uploadImage(@RequestParam("image") MultipartFile file)
+            throws IOException {
+
+        imageRepository.save(Image.builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .image(ImageUtility.compressImage(file.getBytes())).build());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ImageUploadResponse("Image uploaded successfully: " +
+                        file.getOriginalFilename()));
+    }
+
+    @GetMapping(path = {"/image/info/{name}"})
+    public Image getImageDetails(@PathVariable("name") String name) throws IOException {
+
+        final Optional<Image> dbImage = imageRepository.findByName(name);
+
+        return Image.builder()
+                .name(dbImage.get().getName())
+                .type(dbImage.get().getType())
+                .image(ImageUtility.decompressImage(dbImage.get().getImage())).build();
+    }
+
+    @GetMapping(path = {"/image/{name}"})
+    public ResponseEntity<byte[]> getImage(@PathVariable("name") String name) throws IOException {
+
+        final Optional<Image> dbImage = imageRepository.findByName(name);
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.valueOf(dbImage.get().getType()))
+                .body(ImageUtility.decompressImage(dbImage.get().getImage()));
+    }
+}
