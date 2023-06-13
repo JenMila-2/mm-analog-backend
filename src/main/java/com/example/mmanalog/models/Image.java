b517e29d727aec201243a87cfd7935c2984f036a
jensyfranco <jenmilafran@gmail.com>
2023-06-13 11:32:26 +0200

    feat: add image entity and image repository for image upload function

---
+++ src/main/java/com/example/mmanalog/models/Image.java
@@ -0,0 +1,38 @@
+package com.example.mmanalog.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "image")
+@Data
+@Builder
+public class Image {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "image", unique = false, nullable = false, length = 100000)
+    private byte[] image;
+}
