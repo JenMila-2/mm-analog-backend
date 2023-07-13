7a57935ebac21d77327632f269e760785e63cfa9
jensyfranco <jenmilafran@gmail.com>
2023-07-13 09:25:06 +0200

    docs: update data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,17 +3,19 @@ INSERT INTO users (username, name, email, password, enabled)
 VALUES
     ('Pedro20', 'Pedro Perez','pedro@mail.com', '$2a$12$rBImbI7IECYBlcDSdUIXu.m4yPC8q15W1N4q5BaZtF0z2H6dRjaE2', true),
     ('Marisol2023', 'Marisol Lopez', 'marisol@mail.com', '$2a$12$YGKrv7J7xvuajKHXY/GtYOdyElM2riBOZgxBbhK1ZEOepfDvWAD5.', true),
-    ('Jensy2222', 'Jensy Franco', 'jensyhotography@mail.com', '$2a$12$rYbeEqmxx.ElKC5PimWc1OVccCAJwy4Qyki93yc8Y8cHjhGHuMpuu', true),
+    ('Jensy2222', 'Jensy Franco', 'jensyphotography@mail.com', '$2a$12$rYbeEqmxx.ElKC5PimWc1OVccCAJwy4Qyki93yc8Y8cHjhGHuMpuu', true),
     ('Benniezzzzzz80', 'Scott Bennie', 'bennie@mail.com', '$2a$12$h8YyS5u4hMHCC8E7tEMODetux9cZxZqgQf2UH/JVCfnK4kpdseusG', true),
-    ('Admin2', 'Administrator', 'admin@mail.com', '$2a$12$lzHg9K/PNrkjzUCJUUWol.ojbHuHwCbbNueirHxVWY5GKEGAId0vi', true);
+    ('Admin2', 'Administrator', 'admin@mail.com', '$2a$12$lzHg9K/PNrkjzUCJUUWol.ojbHuHwCbbNueirHxVWY5GKEGAId0vi', true),
+    ('User2', 'Test User', 'user@mail.com', '$2a$12$ILqWKVyZIThtkt61gPCfLeQHuGuHFnxkC7CDSSAOCuCXjZOPfdopS', true);
 
 INSERT INTO authorities (username, authority) VALUES ('Pedro20', 'ROLE_USER');
 INSERT INTO authorities (username, authority) VALUES ('Marisol2023', 'ROLE_USER');
 INSERT INTO authorities (username, authority) VALUES ('Benniezzzzzz80', 'ROLE_USER');
-INSERT INTO authorities (username, authority) VALUES ('Jensy2222', 'ROLE_ADMIN');
+INSERT INTO authorities (username, authority) VALUES ('Jensy2222', 'ROLE_USER');
+INSERT INTO authorities (username, authority) VALUES ('Admin2', 'ROLE_ADMIN');
+INSERT INTO authorities (username, authority) VALUES ('User2', 'ROLE_USER');
 
-
---Film Stock Inventory user 100
+--Film Stock Inventory user Pedro20
 INSERT INTO filmstockinventories (id, film_stock_name, remaining_rolls, brand, stock, format, iso, development_process, storage, rolls_shot, film_expiration_date, username)
 VALUES
     (100, 'Professional T-Max 100', 5, 'Kodak', 'T-Max 100', '120 film (Medium)', 100, 'Black & White', 'Freezer', 3, '2024-01-01', 'Pedro20'),
@@ -23,7 +25,7 @@ VALUES
     (104, 'AgfaPhoto APX 100', 4, 'Ilford', ' AgfaPhoto', '35mm', 100, 'Black & White', 'Freezer', 3, '2023-11-01', 'Pedro20');
 
 
---Film Stock Inventory user 101
+--Film Stock Inventory user Marisol2023
 INSERT INTO filmstockinventories (id, film_stock_name, remaining_rolls, brand, stock, format, iso, development_process, storage, rolls_shot, film_expiration_date, username)
 VALUES
     (105, 'Kodak Portra 160', 4, 'Kodak', 'Portra 160', '35mm', 160, 'C-41 Color', 'Freezer', 2, '2024-06-01', 'Marisol2023'),
@@ -32,7 +34,7 @@ VALUES
     (108, 'CineStill 800T', 4, 'CineStill', 'CS 800T', '35mm', 800, 'C-41 Color', 'Freezer', 2, '2025-05-01', 'Marisol2023'),
     (109, 'Ilford HP5 Plus', 10, 'Ilford', 'HP5Plus', '35mm', 400, 'Black & White', 'Freezer', 6, '2026-01-20', 'Marisol2023');
 
---Film Stock Inventory user 102
+--Film Stock Inventory user Jensy2222
 INSERT INTO filmstockinventories (id, film_stock_name, remaining_rolls, brand, stock, format, iso, development_process, storage, rolls_shot, film_expiration_date, username)
 VALUES
     (110, 'Lomo Lady Grey 400', 4, 'Lomography', 'Lady Grey 400', '35mm', 400, 'Black & White', 'Freezer', 1, '2023-06-16', 'Jensy2222'),
@@ -41,15 +43,23 @@ VALUES
     (113, 'Kodak Gold', 3, 'Kodak', 'Gold 200', '35mm', 200, 'C-41 Color', 'Freezer', 2, '2024-04-10', 'Jensy2222'),
     (114, 'Ilford Delta 3200', 2, 'Ilford', 'Delta3200', '35mm', 3200, 'Black & White', 'Freezer', 4, '2025-04-20', 'Jensy2222');
 
---Film Stock Inventory user 103
+--Film Stock Inventory user Benniezzzzzz80
 INSERT INTO filmstockinventories (id, film_stock_name, remaining_rolls, brand, stock, format, iso, development_process, storage, rolls_shot, film_expiration_date, username)
 VALUES
     (115, 'Professional RO 400H 120 film', 6, 'Fujifilm', 'PRO 400H', '120 film (Medium)', 400, 'C-41 Color', 'Freezer', 4, '2022-04-25', 'Benniezzzzzz80'),
-    (116, 'Lomography Color Negative 800', 4, ' Lomography ', ' 800 Color Negative', '35mm', 800, 'C-41 Color', 'Freezer', 6, '2024-04-20', 'Benniezzzzzz80'),
-    (117, 'CineStill BwXX ', 2, 'CineStill', ' CineStill BwXX ', '35mm', 200, 'Black & White', 'Freezer', 0, '2023-12-01', 'Benniezzzzzz80'),
+    (116, 'Lomography Color Negative 800', 4, 'Lomography', ' 800 Color Negative', '35mm', 800, 'C-41 Color', 'Freezer', 6, '2024-04-20', 'Benniezzzzzz80'),
+    (117, 'CineStill BwXX ', 2, 'CineStill', 'CineStill BwXX ', '35mm', 200, 'Black & White', 'Freezer', 0, '2023-12-01', 'Benniezzzzzz80'),
     (118, 'Professional Kodak Tri-X 400 ', 8, 'Kodak', 'Tri-X 400', '35mm', 400, 'Black & White', 'Freezer', 7, '2025-02-20', 'Benniezzzzzz80'),
-    (119, ' Professional Kodak Tri-X 400 ', 6, 'Kodak', 'Tri-X 400', '120 film (Medium)', 400, 'Black & White', 'Freezer', 9, '2026-02-01', 'Benniezzzzzz80');
+    (119, 'Professional Kodak Tri-X 400 ', 6, 'Kodak', 'Tri-X 400', '120 film (Medium)', 400, 'Black & White', 'Freezer', 9, '2026-02-01', 'Benniezzzzzz80');
 
+--Film Stock Inventory user User2
+INSERT INTO filmstockinventories (id, film_stock_name, remaining_rolls, brand, stock, format, iso, development_process, storage, rolls_shot, film_expiration_date, username)
+VALUES
+    (120, 'Kodak Gold', 8, 'Kodak', 'Gold 200', '35mm', 400, 'C-41 Color', 'Freezer', 4, '2023-04-25', 'User2'),
+    (121, 'Kodak Portra 400', 4, 'Kodak', 'Portra 400', '35mm', 400, 'C-41 Color', 'Freezer', 6, '2024-04-20', 'User2'),
+    (122, 'Ilford HP5 Plus', 6, 'Ilford', 'HP5Plus', '35mm', 400, 'Black & White', 'Freezer', 10, '2025-01-20', 'User2'),
+    (123, 'Kodak Ultra Max', 5, 'Kodak', 'Ultra Max', '35mm', 400, 'C-41 Color', 'Freezer', 9, '2026-01-01', 'User2'),
+    (124, 'Lomo Lady Grey 400', 6, 'Lomography', 'Lady Grey 400', '35mm', 400, 'Black & White', 'Freezer', 3, '2023-05-01', 'User2');
 
 --Project Folders
 INSERT INTO projectfolders (id, project_title, project_concept, username)
@@ -61,7 +71,9 @@ VALUES
     (105, 'In between dreams', 'A poetic, photographic meditation on the subtlety of everyday life. Ongoing project.', 'Jensy2222'),
     (106, 'The Constructed Self', 'Collection of self portraits.', 'Jensy2222'),
     (107, 'At midnight', 'Photographing the streets of London by night.', 'Benniezzzzzz80'),
-    (108, 'Family Portraits', 'Photographers of different families. Ongoing project.', 'Benniezzzzzz80');
+    (108, 'Family Portraits', 'Photographers of different families. Ongoing project.', 'Benniezzzzzz80'),
+    (109, 'Project color', 'Collection of photos shot with Kodak Portra 400.', 'Benniezzzzzz80'),
+    (110, 'Project B&W', 'Collection of B&W portraits.', 'Benniezzzzzz80');
 
 --Photo Logs user Jensy2222
 INSERT INTO photologs (id, photo_title, camera, film_stock, film_format, shot_at_iso, aperture, shutter_speed, exposure_compensation, date_taken, developed_by_lab, notes, folder_id, username)
@@ -80,6 +92,19 @@ VALUES
     (251, 'Night_2', 'Nikon F3','CineStill 800T', '35mm', 800, 'f8', '1/60', '+2', '2023-02-01', 'Ontwikkelservice Foto Verweij', 'Night photo walk', 107, 'Benniezzzzzz80'),
     (252, 'Night_3', 'Nikon F3','CineStill 800T', '35mm', 800, 'f8', '1/60', '+2', '2023-02-01', 'Ontwikkelservice Foto Verweij', 'Night photo walk', 107, 'Benniezzzzzz80');
 
+--Photo Logs user User2
+INSERT INTO photologs (id, photo_title, camera, film_stock, film_format, shot_at_iso, aperture, shutter_speed, exposure_compensation, date_taken, developed_by_lab, notes, folder_id, username)
+VALUES
+    (400, 'Color_1', 'Canon AE-1','Kodak Portra 400', '35mm', 400, 'f5.6', '1/60', '+1', '2023-04-01', 'Ontwikkelservice Foto Verweij', 'Shot on a trip to Paris', 109, 'User2'),
+    (401, 'Color_2', 'Canon AE-1','Kodak Portra 400', '35mm', 400, 'f8', '1/60', '0', '2023-04-01', 'Ontwikkelservice Foto Verweij', 'Shot on a trip to Paris', 109, 'User2'),
+    (402, 'Color_3', 'Canon AE-1','Kodak Portra 400', '35mm', 400, 'f8', '1/60', '0', '2023-04-01', 'Ontwikkelservice Foto Verweij', 'Shot on a trip to Paris', 109, 'User2');
+
+--Film Development logs user Jensy2222
 INSERT INTO filmdevelopmentlogs (id, roll_name, project, camera, film_stock, film_format, shot_at_iso, development_process, status, roll_started, roll_finished, exposed, developed, scanned, developed_by_lab, username)
 VALUES
     (300, 'Roll 101 - Kodak Ultra Max', 'Varia, no specific project','Minolta Riva Zoom 115', 'Kodak Ultra Max', '35mm', 400, 'C-41 Color', 'Done', '2023-04-01', '2023-04-20', true, true, true, 'Ontwikkelservice Foto Verweij', 'Jensy2222');
+
+--Film Development logs user User2
+INSERT INTO filmdevelopmentlogs (id, roll_name, project, camera, film_stock, film_format, shot_at_iso, development_process, status, roll_started, roll_finished, exposed, developed, scanned, developed_by_lab, username)
+VALUES
+    (400, 'Roll 10 - Kodak Portra', 'Project Color','Canon AE-1', 'Kodak Portra 400', '35mm', 400, 'C-41 Color', 'Done', '2023-04-01', '2023-05-01', true, true, true, 'Ontwikkelservice Foto Verweij', 'User2');
